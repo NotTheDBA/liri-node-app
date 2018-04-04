628348ebc97d6377139f0e3c2e7eaa1e10d31521
4RSYS\rreitz <rreitz@4rsystems.com>
2018-04-04 11:02:27 -0400

    Added some regions for readability.

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,9 +1,15 @@
+//#region Requires
 // Read and set environment variables
 require("dotenv").config();
 
+var request = require("request");
+//#endregion
+
+//#region Confirmations
 // prints `our key` to the console
 console.log(process.env.TWITTER_CONSUMER_KEY)
 
 // prints `our secret` to the console
 console.log(process.env.TWITTER_CONSUMER_SECRET)
-    // etc.
+
+//#endregion
