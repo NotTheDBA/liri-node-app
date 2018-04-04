d80f50e873a3ca0d2f29929fc7388f4d8d5308cd
4RSYS\rreitz <rreitz@4rsystems.com>
2018-04-04 13:56:52 -0400

    Added skeleton code.

--- liri.js
+++ liri.js
@@ -1,9 +1,17 @@
+import { spotify } from "./keys.js";
+
 //#region Requires
 // Read and set environment variables
 require("dotenv").config();
 var request = require("request");
 var Twitter = require('twitter');
 var keys = require("./keys.js");
+//#endregion
+
+//#region Read Arguments
+var command = process.argv[2];
+var option1 = process.argv[3];
+
 //#endregion
 
 //#region variables
@@ -13,33 +21,66 @@ var client = new Twitter(keys.twitter);
 
 //#region Confirmations
 // prints `your key` to the console
-console.log(process.env.TWITTER_CONSUMER_KEY)
+// console.log(process.env.TWITTER_CONSUMER_KEY)
 
 // prints `your spotify id` to the console
 // console.log(process.env.SPOTIFY_ID)
 
 //#endregion
 
-//#region pseudo-code
-/**
- * Make it so liri.js can take in one of the following commands:
+//#region Command Switch
+switch (command) {
 
-    * `my-tweets`
+    case 'my-tweets':
+        getTweets();
+        break;
 
-    * `spotify-this-song`
+    case 'spotify-this-song':
+        spotify(option1);
+        break;
 
-    * `movie-this`
 
-    * `do-what-it-says`
- */
+    case "movie-this":
+        movie(option1);
+        break;
 
+    case "do-what-it-says":
+        doEeet(option1);
+        break;
+
+    default:
+        console.log("Sorry, I don't know how to '" + command + "'")
+}
 //#endregion
 
-//#region Twitter
-var params = { screen_name: 'NotTheDBABot' };
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
-    }
-});
+//#region My Tweets
+function getTweets() {
+    var params = { screen_name: 'NotTheDBABot' };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+}
+//#endregion
+
+//#region Spotify This Song
+function spotify(option1) {
+
+    console.log("I don't know how to 'spotify-this-song'... yet!")
+}
+//#endregion
+
+//#region Movie This
+function movie(option1) {
+    console.log("I don't know how to 'movie-this'... yet!")
+
+}
+//#endregion
+
+//#region Do What It Says
+function doEeet(option1) {
+    console.log("I don't know how to 'do-what-it-says'... yet!")
+
+}
 //#endregion
