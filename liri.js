d8887c947bf15a71f8412c4b4273a42a4f951b4f
Randy Reitz <rsreitz@outlook.com>
2018-04-07 12:41:20 -0400

    Random command now supports multiple inputs from the file.

--- liri.js
+++ liri.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 var request = require("request");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var fs = require("fs");
+// var fs = require("fs");
+var lineReader = require('line-reader');
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
@@ -115,7 +116,9 @@ function spotifyThis(option1) {
 
 //#region Movie This
 function movieThis(option1) {
-
+    if (typeof option1 === "undefined") {
+        option1 = "Mr. Nobody"
+    }
     var queryUrl = "http://www.omdbapi.com/?t=" + option1 + "&y=&plot=short&apikey=trilogy"
 
     request(queryUrl, function(error, response, body) {
@@ -151,16 +154,10 @@ function movieThis(option1) {
 //#endregion
 
 //#region Do What It Says
-function doThis(option1) {
+function doThis() {
 
-
-    fs.readFile("random.txt", "utf8", function(err, data) {
-
-        if (err) {
-            return err;
-        }
-
-        var comOpts = data.split(",")
+    lineReader.eachLine('random.txt', function(line, last) {
+        var comOpts = line.split(",")
         console.log("Selecting command...")
         console.log("Running command...")
         console.log("Command: " + comOpts[0] + "...")
